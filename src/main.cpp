ede868b9954bb425c568571abc93427223bfa4b7
Nigel Andrews <nigel.andrews@epita.fr>
2024-03-15 21:43:58 +0100

    feat(vector): basic vec3 class

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+#include "vector/vector.h"
+
+using namespace raytracer;
+
 int main()
 {
     std::cout << "Hello World !\n";
+
+    Vec3 test = { 1, 1, 1 };
+
+    std::cout << test << std::endl;
 
     return 0;
 }
---
+++ src/vector/vector.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <cmath>
+#include <ostream>
+#ifdef __cpp_lib_print
+#    include <print>
+#else
+#    include <format>
+#endif // ! __cpp_lib_print
+
+namespace raytracer
+{
+    class Vec3
+    {
+    public:
+        Vec3() = default;
+
+        Vec3(float x, float y, float z)
+            : x_(x)
+            , y_(y)
+            , z_(z)
+        {}
+
+        float norm() const
+        {
+            return std::sqrt(this->x_ * this->x_ + this->y_ * this->y_
+                             + this->z_ * this->z_);
+        }
+
+        Vec3 normalize() const
+        {
+            float len = this->norm();
+
+            return { this->x_ / len, this->y_ / len, this->z_ / len };
+        }
+
+        Vec3 operator+(const Vec3& rhs)
+        {
+            return { this->x_ + rhs.x_, this->y_ + rhs.y_, this->z_ + rhs.z_ };
+        }
+
+        Vec3 operator-() const
+        {
+            return { -this->x_, -this->y_, -this->z_ };
+        }
+
+        Vec3 operator-(const Vec3& rhs)
+        {
+            return *this + -rhs;
+        }
+
+        Vec3& operator+=(const Vec3& rhs)
+        {
+            this->x_ += rhs.x_;
+            this->y_ += rhs.y_;
+            this->z_ += rhs.z_;
+
+            return *this;
+        }
+
+        float operator*(const Vec3& rhs) const
+        {
+            return this->x_ * rhs.x_ + this->y_ * rhs.y_ + this->z_ * rhs.z_;
+        }
+
+        Vec3 operator*(float l) const
+        {
+            return { this->x_ * l, this->y_ * l, this->z_ * l };
+        }
+
+        Vec3& operator*=(float l)
+        {
+            this->x_ *= l;
+            this->y_ *= l;
+            this->z_ *= l;
+
+            return *this;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const Vec3& rhs)
+        {
+#ifdef __cpp_lib_print
+            std::print(os,
+#else // ! __cpp_lib_print
+            os << std::format(
+#endif // __cpp_lib_print
+                       "({}, {}, {})", rhs.x_, rhs.y_, rhs.z_);
+
+            return os;
+        }
+
+    private:
+        float x_;
+        float y_;
+        float z_;
+    };
+} // namespace raytracer
