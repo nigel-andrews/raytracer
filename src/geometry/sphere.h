1b7d903853ce4d39f8544221761682150fef56db
Nigel Andrews <nigel.andrews@epita.fr>
2024-03-17 20:36:10 +0100

    feat(geometry): sphere class

---
+++ src/geometry/sphere.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "vector.h"
+
+// TODO: Make this a subclass of object
+class Sphere
+{
+    Sphere(const Vec3& position, float radius = 1.f)
+        : position_(position)
+        , radius_(radius)
+    {}
+
+    bool intersect(const Vec3& ray) const;
+    Vec3 normal_get(const Vec3& point) const;
+
+    // TODO: object materials
+
+private:
+    Vec3 position_;
+    float radius_;
+};
