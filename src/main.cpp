c16004b1a858ee13ef4112093126016d65b4f204
Nigel Andrews <nigel.andrews@epita.fr>
2024-03-24 13:05:24 +0100

    feat: remove SDL2
    Annoying to use and leaked a lot. Considering IMGUI as a replacement

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,17 @@
-// #include <iostream>
-
-#include "scene/camera.h"
-// #include "scene/light.h"
-// #include "window.h"
+#include "scene/scene.h"
 
 int main(void)
 {
-    // if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0)
-    //     std::cout << SDL_GetError();
-    //
-    // {
-    //     int w = 800;
-    //     int h = 600;
-    //
-    //     Window window("Raytracer", w, h,
-    //                   SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
-    //
-    //     if (!window)
-    //         std::cout << SDL_GetError();
-    //
-    //     bool run = true;
-    //     SDL_Event event;
-    //
-    //     while (run)
-    //     {
-    //         if (!SDL_PollEvent(&event))
-    //             continue;
-    //
-    //         switch (event.type)
-    //         {
-    //         case SDL_KEYDOWN:
-    //             run = event.key.keysym.sym != SDLK_ESCAPE;
-    //             break;
-    //         case SDL_QUIT:
-    //             run = false;
-    //             break;
-    //         default:
-    //             break;
-    //         }
-    //
-    //         // Compute and draw
-    //         window.draw();
-    //     }
-    // }
-    //
-    // std::cout << "Quitting\n";
-    // SDL_Quit();
+    int w = 800;
+    int h = 600;
 
-    Camera camera{ 0.5f, M_PI_2, { 0, 0, 0 }, { 0, 0, 1 }, { 0, 1, 0 } };
-    camera.shoot_rays(800, 600);
+    Scene scene{ Camera{ 0.5f,
+                         M_PI_2,
+                         { 0.f, 0.f, 0.f },
+                         { 0.f, 0.f, 1.f },
+                         { 0.f, 1.f, 0.f } } };
+    scene.add_object(Sphere{ { 0.f, 0.f, 10.f }, 3.f });
+    scene.draw_scene(w, h);
 
     return 0;
 }
